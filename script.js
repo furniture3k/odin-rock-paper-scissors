b4f6b1f704bc6b7edbb81b79fca42802427f90c7
furniture3k <130829879+furniture3k@users.noreply.github.com>
2025-12-13 11:31:52 +0200

    Created getComputerChoice function in script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function getComputerChoice(random) {
-    
+function getComputerChoice(num) {
+    const choices = {
+        0: "Rock",
+        1: "Paper", 
+        2: "Scissors"
+    };
+
+    num = Math.floor(Math.random() * 3);
+    return choices[num];
 }
+
+console.log(getComputerChoice())
