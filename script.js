c91a45e4ab4171812149dd4572c64b41eebe6548
furniture3k <130829879+furniture3k@users.noreply.github.com>
2025-12-14 22:38:51 +0200

    Finished rock paper scissors win case logic

--- script.js
+++ script.js
@@ -26,5 +26,17 @@ function playRound(humanChoice, computerChoice) {
         console.log("Your score: " + humanScore);
         console.log("Computer score: " + computerScore);
 
-    } 
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
+        console.log(`You won! You played ${humanChoice} and the computer played ${computerChoice}`);
+        console.log("Your score: " + humanScore);
+        console.log("Computer score: " + computerScore);
+    } else {
+        computerScore++;
+        console.log(`You lost! You played ${humanChoice} and the computer played ${computerChoice}`);
+        console.log("Your score: " + humanScore);
+        console.log("Computer score: " + computerScore);
+    }
 }
